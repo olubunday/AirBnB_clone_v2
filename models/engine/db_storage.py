b39b55a75b34d169ebcd1de14d63300496df55df
Akinlowo Olumide <adeolaolumide9@gmail.com>
2023-07-23 16:25:49 -0700

    Web Frame Work

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,38 +4,33 @@
 
 import models
 import models.base_model
-import models.engine.storage
 import os
 import sqlalchemy
 import sqlalchemy.orm
 
 
-class DBStorage (models.engine.storage.Storage):
+class DBStorage:
     """Store AirBnB clone data model objects in an SQL database"""
 
     __engine = None
     __session = None
     __sessionMaker = None
 
-    def __contains__(self, obj):
-        """Check if an object exists in storage"""
-
-        return obj in DBStorage.__session
-
     def __init__(self):
         """Initialize the database storage engine (use like a singleton)"""
 
-        DBStorage.__engine = sqlalchemy.create_engine(
+        databaseName = os.getenv('HBNB_MYSQL_DB', '')
+        self.__engine = sqlalchemy.create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
-                os.getenv('HBNB_MYSQL_USER'),
-                os.getenv('HBNB_MYSQL_PWD'),
-                os.getenv('HBNB_MYSQL_HOST'),
-                os.getenv('HBNB_MYSQL_DB')
+                os.getenv('HBNB_MYSQL_USER', ''),
+                os.getenv('HBNB_MYSQL_PWD', ''),
+                os.getenv('HBNB_MYSQL_HOST', ''),
+                databaseName
             ),
             pool_pre_ping=True
         )
-        if os.getenv('HBNB_ENV') == 'test':
-            models.base_model.Base.metadata.drop_all(DBStorage.__engine)
+        if os.getenv('HBNB_ENV', '') == 'test':
+            models.base_model.Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Return a collection of objects, optionally filtered by class"""
@@ -56,31 +51,30 @@ class DBStorage (models.engine.storage.Storage):
         return ret
 
     def close(self):
-        """Remove the session and create a new one"""
-
+        """Remove current session and make a new one"""
         DBStorage.__sessionMaker.remove()
         DBStorage.__session = None
         self.reload()
 
     def delete(self, obj=None):
-        """Delete obj from the database"""
+        """Delete obj from the database
 
-        DBStorage.__session.delete(obj)
+        Args:
+            obj (BaseModel): object to remove from the database
 
-    def get(self, cls, id):
-        """Retrieve an object from storage"""
+        """
 
-        if not isinstance(cls, type):
-            cls = models.classes[cls]
-        obj = DBStorage.__session.query(cls).filter(cls.id == str(id)).first()
-        if obj is None:
-            raise KeyError('object with given class and ID not found')
-        return obj
+        self.__session.delete(obj)
 
     def new(self, obj):
-        """Save a new object to the database"""
+        """Save a new object to the database
 
-        DBStorage.__session.add(obj)
+        Args:
+            obj (BaseModel): new object to save
+
+        """
+
+        self.__session.add(obj)
 
     def reload(self):
         """Open a new MySQL session and create tables if necessary"""
@@ -90,21 +84,10 @@ class DBStorage (models.engine.storage.Storage):
             DBStorage.__sessionMaker = sqlalchemy.orm.scoped_session(
                 sqlalchemy.orm.sessionmaker(bind=self.__engine)
             )
-        if DBStorage.__session is None:
-            DBStorage.__session = \
-                DBStorage.__sessionMaker(expire_on_commit=False)
+        if self.__session is None:
+            DBStorage.__session = self.__sessionMaker(expire_on_commit=False)
 
     def save(self):
         """commit all current pending changes to the database"""
 
-        DBStorage.__session.commit()
-
-    def tryGet(self, cls, id, default):
-        """Try to get an object with a fallback value"""
-
-        if not isinstance(cls, type):
-            cls = models.classes[cls]
-        obj = DBStorage.__session.query(cls).filter(cls.id == str(id)).first()
-        if obj is None:
-            return default
-        return obj
+        self.__session.commit()
--- models/state.py
+++ models/state.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
 """This is the state class"""
-
-
-import models
-import models.base_model
-import sqlalchemy
-import sqlalchemy.orm
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from models.city import City
+from sqlalchemy.orm import relationship
 import os
+import models
 
 
-class State(models.base_model.BaseModel, models.base_model.Base):
+class State(BaseModel, Base):
     """This is the class for State"""
-
     __tablename__ = "states"
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = sqlalchemy.Column(
-            'name',
-            sqlalchemy.String(128),
-            nullable=False
-        )
-        cities = sqlalchemy.orm.relationship("City", back_populates="state")
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", back_populates="state")
     else:
         name = ""
 
         @property
         def cities(self):
             """Return all the cities"""
-
-            ret = [
-                city
-                for city in models.storage.all('City').values()
-                if city.state_id == self.id
-            ]
-            return ret
+            city_list = [i for i in models.storage.all(City).values()
+                         if i.state_id == self.id]
+            return city_list
